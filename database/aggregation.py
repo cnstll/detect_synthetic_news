6b008e8d7207b6877af95057ec2d3f9fd4b0435c
cnstll <calle@student.42.fr>
2023-09-22 18:18:26 +0200

    changing table names in the aggregation query

--- database/aggregation.py
+++ database/aggregation.py
@@ -10,32 +10,32 @@ def aggregate_per_sources(db: Session):
             models.NewsArticle.article_source,
             func.sum(
                 case([(models.NewsArticle.title_detection_label == "Fake", 1)], else_=0)
-            ).label("sum_of_fake_title"),
+            ).label("sum_of_fake_titles"),
             func.sum(
                 case([(models.NewsArticle.title_detection_label == "Real", 1)], else_=0)
-            ).label("sum_of_real_title"),
+            ).label("sum_of_real_titles"),
             func.sum(
                 case(
                     [(models.NewsArticle.description_detection_label == "Fake", 1)],
                     else_=0,
                 )
-            ).label("sum_of_fake_description"),
+            ).label("sum_of_fake_descriptions"),
             func.sum(
                 case(
                     [(models.NewsArticle.description_detection_label == "Real", 1)],
                     else_=0,
                 )
-            ).label("sum_of_real_description"),
+            ).label("sum_of_real_descriptions"),
             func.sum(
                 case(
                     [(models.NewsArticle.content_detection_label == "Fake", 1)], else_=0
                 )
-            ).label("sum_of_fake_content"),
+            ).label("sum_of_fake_contents"),
             func.sum(
                 case(
                     [(models.NewsArticle.content_detection_label == "Real", 1)], else_=0
                 )
-            ).label("sum_of_real_content"),
+            ).label("sum_of_real_contents"),
             func.avg(
                 case(
                     [
@@ -68,7 +68,7 @@ def aggregate_per_sources(db: Session):
                     ],
                     else_=None,
                 )
-            ).label("average_score_for_fake_description"),
+            ).label("average_score_for_fake_descriptions"),
             func.avg(
                 case(
                     [
@@ -79,7 +79,7 @@ def aggregate_per_sources(db: Session):
                     ],
                     else_=None,
                 )
-            ).label("average_score_for_real_description"),
+            ).label("average_score_for_real_descriptions"),
             func.avg(
                 case(
                     [
@@ -90,7 +90,7 @@ def aggregate_per_sources(db: Session):
                     ],
                     else_=None,
                 )
-            ).label("average_score_for_fake_content"),
+            ).label("average_score_for_fake_contents"),
             func.avg(
                 case(
                     [
@@ -101,7 +101,7 @@ def aggregate_per_sources(db: Session):
                     ],
                     else_=None,
                 )
-            ).label("average_score_for_real_content"),
+            ).label("average_score_for_real_contents"),
         )
         .group_by(models.NewsArticle.article_source)
         .all()
