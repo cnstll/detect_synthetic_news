07179b0acc58f466003633d39bf9e467c4b7ab70
cnstll <calle@student.42.fr>
2023-09-26 17:57:50 +0200

    sync articles with a query str, sources list and go through all the pages of the fetched articles

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import crud, database, models, schemas, aggregation
 from news_api import request_articles
-from processing import process_articles
+from processing import labelize, pre_process
+from math import ceil
 
+MAX_NUMBER_OF_PAGES_TO_FETCH = 50
+TOPIC_SEARCHED = "artificial intelligence"
+SOURCES_LANGUAGE = "en"
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
@@ -47,29 +51,48 @@ async def sync_news_articles(db: Session = Depends(get_db)):
 
 @app.get(path="/sync-articles")
 async def sync_articles_from_query(db: Session = Depends(get_db)):
-    response = request_articles.fetch_all_news_articles(
-        query="artificial intelligence", language="en"
+    initial_response = request_articles.fetch_news_articles_based_on_query(
+        query=TOPIC_SEARCHED, language=SOURCES_LANGUAGE, page=1
     )
-    if response["status"] == "error":
-        return {"status": response["status"], "msg": response["message"]}
+    if initial_response["status"] == "error":
+        return {
+            "status": initial_response["status"],
+            "msg": initial_response["message"],
+        }
     else:
-        _, _, articles = response.values()
-        articles = process_articles.process_articles(articles)
-        # Verify if article already exists in db
+        _, nbrArticles, articles = initial_response.values()
+        nb_pages_to_query = ceil(nbrArticles / 100)
+        print(f"{nb_pages_to_query} pages to query")
+        print(f"{nbrArticles} expected...")
+        print(f"{len(articles)} articles are going to be processed...")
+        for p in range(2, nb_pages_to_query + 1):
+            response = request_articles.fetch_news_articles_based_on_query(
+                query=TOPIC_SEARCHED, language=SOURCES_LANGUAGE, page=p
+            )
+            if response["status"] == "ok":
+                articles += response["articles"]
+            print(f"Added {len(response['articles'])} articles to be processed")
+        # selection and cleaning of articles metadata
+        pre_processed_articles = pre_process.select_and_prepare_articles(articles)
+        # Verify if articles already exists in db
         batched_articles = []
-        for article in articles:
+        for article in pre_processed_articles:
             if crud.get_article_by_id(db, article["article_id"]) is None:
                 batched_articles.append(article)
-        print(f"\nNum of synced articles is {len(batched_articles)}\n")
-        return crud.batch_create_articles(db, batched_articles)
+        # Labelize metadata from articles into fake / real
+        print(f"\nNum of articles sent for labelization is {len(batched_articles)}\n")
+        enriched_articles = labelize.labelize_articles(batched_articles)
+        print(f"\nNum of synced articles is {len(enriched_articles)}\n")
+        return crud.batch_create_articles(db, enriched_articles)
 
 
-@app.get(path="/articles", response_model=list[schemas.NewsArticle])
-async def get_all_available_articles(db: Session = Depends(get_db)):
-    return crud.get_all_articles(db, limit=100)
+@app.get(path="/articles/", response_model=list[schemas.NewsArticle])
+async def get_available_articles(
+    db: Session = Depends(get_db), source: str = "techcrunch", limit: int = 10
+):
+    return crud.get_all_articles(db, limit=limit)
 
 
-# TODO : Endpoint for calculating fake / real distribution per source
 @app.get(path="/articles/get-distribution-fake-real-per-source")
 async def get_distribution_fake_real_per_source(db: Session = Depends(get_db)):
     return aggregation.aggregate_per_sources(db)
